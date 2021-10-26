c7df288bdb6fa68d87b13f135b5abaac230e4ed3
haixuanwo <haixuanwo_clark@hotmail.com>
2021-10-26 10:50:21 +0800

    定义类checklist

---
+++ C++_Ruminations/04_chap.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file 04_chap.cpp
+ * @author Clark (haixuanwoTxh@gmail.com)
+ * @brief the checklist of defining a class
+ * @version 0.1
+ * @date 2021-10-26
+ * @copyright Copyright (c) 2021
+ */
+
+
+/***
+ * 1、需要构造函数吗？
+ * 2、数据成员私有吗？
+ * 3、需要无参构造函数吗？
+ * 4、构造函数初始化所有数据成员？
+ * 5、需要析构吗？
+ * 6、需要虚析构吗？
+ * 7、需要复制构造吗？
+ * 8、需要赋值操作符吗？
+ * 9、赋值操作符能正确赋值给自己吗？
+ * 10、需要定义关系操作符吗？
+ * 11、删除数组用delete[]了吗？
+ * 12、复制构造和赋值构造参数类型中加上const了吗？
+ * 13、函数有引用参数，应该是const引用吗？
+ * 14、适当声明成员函数为const了吗？
+ */
